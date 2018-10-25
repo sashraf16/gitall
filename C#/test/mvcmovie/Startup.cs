60f0a07dc62e50eabaf450fa014e44a34c3286e6
sashraf16 <deadlyrang@gmail.com>
2018-10-25 12:35:05 -0500

    movie asp

--- C#/test/mvcmovie/Startup.cs
+++ C#/test/mvcmovie/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace mvcmovie
 {
@@ -21,7 +23,11 @@ namespace mvcmovie
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add framework services
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlite("Data Source=MvcMovie.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
