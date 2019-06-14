6511d164563ab72b78e105fe4096088c2e3a92e6
sashraf16 <deadlyrang@gmail.com>
2019-06-13 20:11:27 -0500

    fullstack
    changes with sheeraz bhai

--- fullstack/asp.net/Models/Todo.cs
+++ fullstack/asp.net/Models/Todo.cs
@@ -1,8 +1,13 @@
-namespace asp.net.Models
-{
-    public class Todo
-    {
+using System;
+
+namespace asp.net.Models {
+    public class Todo {
         public int Id { get; set; }
-        public string Item { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
     }
 }
