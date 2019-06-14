287f230f4ce18ae861d3c15e02366d2a092322be
sashraf16 <deadlyrang@gmail.com>
2019-06-13 22:15:46 -0500

    navbar for azure

--- Azure/Personal Site/FrontEnd/src/app/app.module.ts
+++ Azure/Personal Site/FrontEnd/src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatDividerModule,
+  MatListModule
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DisplayareaComponent } from './displayarea/displayarea.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidebarComponent, DisplayareaComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
